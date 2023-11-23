41cf713c2ce5c126a590fb76c34d455599537476
Ubuntu <juliazh2003@gmail.com>
2023-11-23 19:29:53 +0000

    Update datasets

---
+++ hw_tts/datasets/buffer_dataset.py
@@ -0,0 +1,12 @@
+from torch.utils.data import Dataset
+
+class BufferDataset(Dataset):
+    def __init__(self, buffer):
+        self.buffer = buffer
+        self.length_dataset = len(self.buffer)
+
+    def __len__(self):
+        return self.length_dataset
+
+    def __getitem__(self, idx):
+        return self.buffer[idx]
