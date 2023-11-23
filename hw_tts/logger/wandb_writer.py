90f4cbc019eeb2c778360af0c6ec2cdf10069301
Ubuntu <juliazh2003@gmail.com>
2023-11-23 19:29:40 +0000

    Update logger

--- hw_tts/logger/wandb_writer.py
+++ hw_tts/logger/wandb_writer.py
@@ -1,30 +1,23 @@
 from datetime import datetime
-
 import numpy as np
-import pandas as pd
 import wandb
 
 
 class WanDBWriter:
-    def __init__(self, config, logger):
+    def __init__(self, config):
         self.writer = None
         self.selected_module = ""
 
-        try:
-            import wandb
-            wandb.login()
+        wandb.login()
 
-            if config['trainer'].get('wandb_project') is None:
-                raise ValueError("please specify project name for wandb")
+        if not hasattr(config, 'wandb_project'):
+            raise ValueError("please specify project name for wandb")
 
-            wandb.init(
-                project=config['trainer'].get('wandb_project'),
-                config=config.config
-            )
-            self.wandb = wandb
-
-        except ImportError:
-            logger.warning("For use wandb install it via \n\t pip install wandb")
+        wandb.init(
+            project=getattr(config, 'wandb_project'),
+            config=config
+        )
+        self.wandb = wandb
 
         self.step = 0
         self.mode = ""
@@ -40,34 +33,33 @@ class WanDBWriter:
             self.add_scalar("steps_per_sec", 1 / duration.total_seconds())
             self.timer = datetime.now()
 
-    def _scalar_name(self, scalar_name):
-        return f"{scalar_name}_{self.mode}"
+    def scalar_name(self, scalar_name):
+        return f"{self.mode}/{scalar_name}"
 
     def add_scalar(self, scalar_name, scalar):
         self.wandb.log({
-            self._scalar_name(scalar_name): scalar,
+            self.scalar_name(scalar_name): scalar,
         }, step=self.step)
 
     def add_scalars(self, tag, scalars):
         self.wandb.log({
-            **{f"{scalar_name}_{tag}_{self.mode}": scalar for scalar_name, scalar in
-               scalars.items()}
+            **{f"{scalar_name}_{tag}_{self.mode}": scalar for scalar_name, scalar in scalars.items()}
         }, step=self.step)
 
     def add_image(self, scalar_name, image):
         self.wandb.log({
-            self._scalar_name(scalar_name): self.wandb.Image(image)
+            self.scalar_name(scalar_name): self.wandb.Image(image)
         }, step=self.step)
 
     def add_audio(self, scalar_name, audio, sample_rate=None):
         audio = audio.detach().cpu().numpy().T
         self.wandb.log({
-            self._scalar_name(scalar_name): self.wandb.Audio(audio, sample_rate=sample_rate)
+            self.scalar_name(scalar_name): self.wandb.Audio(audio, sample_rate=sample_rate)
         }, step=self.step)
 
     def add_text(self, scalar_name, text):
         self.wandb.log({
-            self._scalar_name(scalar_name): self.wandb.Html(text)
+            self.scalar_name(scalar_name): self.wandb.Html(text)
         }, step=self.step)
 
     def add_histogram(self, scalar_name, hist, bins=None):
@@ -81,12 +73,8 @@ class WanDBWriter:
         )
 
         self.wandb.log({
-            self._scalar_name(scalar_name): hist
+            self.scalar_name(scalar_name): hist
         }, step=self.step)
-
-    def add_table(self, table_name, table: pd.DataFrame):
-        self.wandb.log({self._scalar_name(table_name): wandb.Table(dataframe=table)},
-                       step=self.step)
 
     def add_images(self, scalar_name, images):
         raise NotImplementedError()
